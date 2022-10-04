26f975688b9037e9dd3929a457bde390129d1c60
Dorota Bjoorn <Dorota.Bjoorn@iths2022>
2022-10-04 09:19:54 +0200

    added comments

--- Code-alongs/Lec14-unit_test/tests/test_vector.py
+++ Code-alongs/Lec14-unit_test/tests/test_vector.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-import sys, os
-import unittest
+import sys, os                              # för att komma åt systemet och operativsystemet, komma åt olika paths och dir
+import unittest                             #
 
 
 print(__file__)
@@ -10,12 +10,13 @@ os.chdir(os.path.dirname(__file__))
 
 # we define a path that is up one step
 # in this path we have vector.py and plotter.py and manual_testing.ipynb
-path_to_vector_module = os.path.abspath("../")
+path_to_vector_module = os.path.abspath("../") # går upp ett steg med ../ och hamnar därmed i Lec14-unit_test mappen
 
-sys.path.append(path_to_vector_module)
+sys.path.append(path_to_vector_module)      #eftersom filerna ligger i olika mappar
 print(path_to_vector_module)
 
-from vector import Vector
+from vector import Vector                   #vara denna skulle funka om testfilen och vektorfilken låg i samma mapp
+                                            # hela vectorfilen körs och därmed improteras plotter och därmed importeras matplotlib tex
 
 
 class TestVector(unittest.TestCase):
